1f7112f0688c6980a2045531893c08575c01bbde
chenyuwen <1525351498@qq.com>
2024-04-16 17:18:55 +0800

    docs: 修改名字

--- android/app/src/main/java/com/awesomeproject/MainActivity.kt
+++ android/app/src/main/java/com/awesomeproject/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "rn_demo"
+  override fun getMainComponentName(): String = "rn_tips"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- ios/rntipsTests/rntipsTests.m
+++ ios/rntipsTests/rntipsTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface rndemoTests : XCTestCase
+@interface rntipsTests : XCTestCase
 
 @end
 
-@implementation rndemoTests
+@implementation rntipsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
