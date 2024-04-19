385e2c0c09d672bf5c31686432d3ced764edf716
chenyuwen <1525351498@qq.com>
2024-04-19 17:59:19 +0800

    config: 增加统一前缀

--- src/pages/dev/devPage/index.tsx
+++ src/pages/dev/devPage/index.tsx
@@ -1,14 +1,16 @@
 import { SafeAreaView, ScrollView, StatusBar, Text } from 'react-native'
-import styles from './styles.scss'
+import styles from '../../../theme/app.scss'
+import { EAppIdPrefix, EModulePrefix } from '../../../theme/config'
 import { Fragment } from 'react'
 
+const PREFIX = `${EAppIdPrefix.MX_ONE}-${EModulePrefix.COMPONENT}`
 const DevPage = (): React.ReactElement => {
   return (
     <Fragment>
       <SafeAreaView>
         <StatusBar />
         <ScrollView contentInsetAdjustmentBehavior="automatic">
-          <Text style={styles.text}>123</Text>
+          <Text style={styles[`${PREFIX}-text`]}>123</Text>
         </ScrollView>
       </SafeAreaView>
     </Fragment>
